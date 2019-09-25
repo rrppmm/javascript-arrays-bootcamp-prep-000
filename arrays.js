022c34299354dcaeef007a970eeb6a494d84673e
Paul M <paulmcadory@gmail.com>
2019-09-25 02:21:17 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,4 +2,6 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(array, element) {
   return [element, ...array];
 }
-function destructivelyAddElementToBeginningOfArray()
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  
+}
