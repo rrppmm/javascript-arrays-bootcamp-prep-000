bc30f47bec082510c18cc4ab328b06077f2d0fc4
Paul M <paulmcadory@gmail.com>
2019-09-25 02:29:08 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -9,3 +9,10 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 function addElementToEndOfArray(array, element) {
   return [...array, element];
 }
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array;
+}
+function accessElementInArray(array, index) {
+  return 
+}
