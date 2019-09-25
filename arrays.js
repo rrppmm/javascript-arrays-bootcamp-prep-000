729d19e394b28c255be9c3b16bef51d0cf8d9c4b
Paul M <paulmcadory@gmail.com>
2019-09-25 02:32:04 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -16,4 +16,6 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
   return array[index];
 }
-function destructively
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.
+}
