4e88f1aae9fbd510c23b2a88173cd19382fe4ac3
Paul M <paulmcadory@gmail.com>
2019-09-25 02:08:42 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,16 +1,1 @@
-var grilledCheeseIngredients = [
-  'bread',
-  'mild cheese',
-  'sharp cheese',
-  'butter',
-  'tomato',
-  'garlic'
-]
- 
-var tomatoSauceIngredients = [
-  'tomato',
-  'garlic',
-  'olive oil',
-  'basil',
-  'oregano'
-]
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
