5a944c2610166f7ebea8a174576c24e7aae2fd06
Paul M <paulmcadory@gmail.com>
2019-09-25 02:03:51 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,6 @@
 var ingredient1 = "bread";
 var ingredient2 = "mild cheese";
 var ingredient3 = "sharp cheese";
-var
+var ingredient4 = "butter";
+var ingredient5 = "tomato";
+var ingredient6 = "garlic";
